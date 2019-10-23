a7c4e6eeef27d34d7893dc9c5ae5bb37656df2e3
AlexPrediger <39220443+AlexPrediger@users.noreply.github.com>
2019-10-23 21:41:09 +0200

    Add Game initialization and paint and timer tick events

--- SpaceInvaders-1.0/Form1.cs
+++ SpaceInvaders-1.0/Form1.cs
@@ -12,14 +12,41 @@ namespace SpaceInvaders_1._0
 {
     public partial class Form1 : Form
     {
+
+        private Game game;
+        bool gameOver = false;
+
         public Form1()
         {
             InitializeComponent();
+
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            // Declare new game, setting game boundaries
+            game = new Game(this.DisplayRectangle);
+
+            // Start the animation timer straight away - animate stars
+            AnimationTimer.Interval = 330;
+            AnimationTimer.Start();
         }
 
         private void AnimationTimer_Tick(object sender, EventArgs e)
         {
+            game.Twinkle();
 
+            // Redraw Form1
+            this.Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Bitmap bitmap = new Bitmap(this.Width, this.Height);
+            Graphics graphics = Graphics.FromImage(bitmap);
+
+            game.Draw(g, gameOver);
+
+            // Copy bitmap image onto Form1 graphics
+            e.Graphics.DrawImageUnscaled(bitmap, 0, 0);
         }
     }
 }
